8220e5aaf7d2756c2929726ef0b8ddb59a117468
Przemyslaw Symber <symber.przemyslaw@gmail.com>
2024-08-26 13:11:54 +0200

    Stable version, mobile ready

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,16 @@
-import { useState } from "preact/hooks";
-import "./app.css";
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { useState } from 'preact/hooks';
+import './app.css';
+import pdfMake from "pdfmake/build/pdfmake.min";
 
-import logo from "./assets/logo_unio.png";
+pdfMake.fonts = {
+    Roboto: {
+      normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
+      bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
+      italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
+      bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
+    }
+ }
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export function App() {
   const [categories, setCategories] = useState([
